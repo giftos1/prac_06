e45a8321eab721ced56893924fa7496a392e07e7
Gift <giftsydney676@gmail.com>
2020-09-17 16:30:23 +1000

    "Define the str method"

--- car.py
+++ car.py
@@ -4,12 +4,13 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, name="", fuel=0, ):
         """Initialise a Car instance.
         fuel: float, one unit of fuel drives one kilometre
         """
         self.fuel = fuel
         self.odometer = 0
+        self.name = name
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
@@ -27,3 +28,8 @@ class Car:
             self.fuel -= distance
         self.odometer += distance
         return distance
+
+    def _str_(self):
+        """Return a formatted display of a car object"""
+        return "{}, fuel={}, odometer={}".format(self.name, self.fuel,
+                                                 self.odometer)
