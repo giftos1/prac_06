35ec4a775aed2253c7052ee6ecb8d09627cc9030
Gift <giftsydney676@gmail.com>
2020-09-17 17:00:47 +1000

    "Check if str method is working by printing as stated in class"

---
+++ languages.py
@@ -0,0 +1,10 @@
+from practical_6.programming_language import ProgrammingLanguage
+
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(visual_basic)
+print(ruby)
+print(python)
+
+
