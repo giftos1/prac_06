4dd3c63fcec165d1dbecabef8983c98ffe60408d
Gift <giftsydney676@gmail.com>
2020-09-17 17:02:46 +1000

    "Display string of objects in init function"

---
+++ practical_6/programming_language.py
@@ -0,0 +1,15 @@
+class ProgrammingLanguage:
+
+    def __init__(self, language, typing, reflection, year):
+        self.language = language
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        """Returns a formatted string of objects"""
+        return "{}, {} Typing, Reflection={}, First appeared in {}".format(
+            self.language, self.typing, self.reflection, self.year)
+
+
+
